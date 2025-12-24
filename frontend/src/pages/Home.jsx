ab5319fe0f0a5662a3bd47db36b1bb9ac14ed942
Xuan Tung Luu <xuantung.brian@gmail.com>
2025-12-24 16:19:15 -0500

    2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,26 @@
 import { useState, useEffect } from 'react'
+import './Home.css'
 
 const Home = () => {
   const [chapterContent, setChapterContent] = useState('Loading')
+  const [chapterId, setChapterId] = useState(26129007)
+
+  const handleClick = (chapterId) => {
+    setChapterId(chapterId => chapterId + 2)
+    setChapterContent('Loading')
+  }
+
   useEffect(() => {
-    fetch('http://localhost:5000/translator/37740/26129003')
+    fetch(`http://localhost:5000/translator/37740/${chapterId}`)
     .then(response => response.json())
     .then(data => setChapterContent(data.data))
     .catch(error => console.log(error));
-  }, [])
+  }, [chapterId])
 
   return (
-    <div>
+    <div className="chapter-content">
       {chapterContent}
+      {chapterContent !== "Loading" && <button onClick={handleClick}>Next chapter</button>}
     </div>
   )
 }
